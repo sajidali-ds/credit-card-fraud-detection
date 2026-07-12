aa0cdbe6b49c40b5aacb0568856d9d19a976f8c3
Sajid Ali <sajidalidatascience@gmail.com>
2026-07-12 09:56:51 +0530

    Add Evaluation metrics

--- app.py
+++ app.py
@@ -14,17 +14,7 @@ st.set_page_config(
     page_icon="💳",
     layout="wide",
 )
-st.write("Current Working Directory:", os.getcwd())
-st.write("BASE_DIR:", config.BASE_DIR)
-st.write("MODELS_DIR:", config.MODELS_DIR)
-st.write("MODELS_DIR exists:", os.path.exists(config.MODELS_DIR))
 
-if os.path.exists(config.MODELS_DIR):
-    st.write("Models folder:", os.listdir(config.MODELS_DIR))
-
-st.write("BEST_MODEL_PATH:", config.BEST_MODEL_PATH)
-st.write("Model exists:", os.path.exists(config.BEST_MODEL_PATH))
-st.write("Scaler exists:", os.path.exists(config.SCALER_PATH))
 
 @st.cache_resource
 def load_model_and_scaler():
